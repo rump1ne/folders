0c8b9479f00bec3f2abc0d3cd2c7e4301d6901b3
Roman <112933914+rump1ne@users.noreply.github.com>
2023-11-30 05:22:51 +0700

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -1,36 +1,62 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
 
-int main() { 
+class NameGenerator {
+private:
+    std::string chars;
+    int count;
+
+public:
+    NameGenerator(const std::string& characters, int itemCount) : chars(characters), count(itemCount) {}
+
+    void GenerateNames(std::ofstream& outputFile) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, chars.size() - 1);
+
+        for (int i = 0; i < count; ++i) {
+            std::string name;
+            for (int j = 0; j < 5; ++j) { // Adjust the length of the generated name as needed
+                name += chars[dis(gen)];
+            }
+            outputFile << name << std::endl;
+        }
+    }
+};
+
+int main() {
     setlocale(LC_ALL, "RUS");
- 
+
     std::string folderChars, fileChars;
-    int folderCount, fileCount; 
- 
+    int folderCount, fileCount;
+
     std::cout << "Введите уникальные символы для названия папок: ";
-    std::cin >> folderChars; 
- 
+    std::cin >> folderChars;
+
     std::cout << "Введите количество папок: ";
-    std::cin >> folderCount; 
- 
+    std::cin >> folderCount;
+
     std::cout << "Введите уникальные символы для названия файлов: ";
     std::cin >> fileChars;
- 
+
     std::cout << "Введите количество файлов: ";
     std::cin >> fileCount;
- 
+
     std::ofstream logFile("log.txt");
-    if (!logFile.is_open()) { 
+    if (!logFile.is_open()) {
         std::cerr << "Ошибка открытия файла лога." << std::endl;
-        return 1; 
-    } 
- 
+        return 1;
+    }
+
     NameGenerator folderNameGenerator(folderChars, folderCount);
     NameGenerator fileNameGenerator(fileChars, fileCount);
- 
+
     folderNameGenerator.GenerateNames(logFile);
-    fileNameGenerator.GenerateNames(logFile); 
- 
+    fileNameGenerator.GenerateNames(logFile);
+
     logFile.close();
- 
+
     return 0;
 }
